ef1647c160537dab66f277483e213fdba578b665
Tony Bowden <tony@tmtm.com>
2021-08-17 20:10:55 +0300

    add scraper for official site

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -7,17 +7,33 @@ require 'pry'
 class MemberList
   class Member
     def name
-      noko.css('.name').text.tidy
+      noko.css('a').text.tidy
     end
 
     def position
-      noko.css('.position').text.tidy
+      return ministry if ministry.start_with? 'Presidente'
+      return ministry.sub('Ministero', 'Ministro') if role == 'Ministro'
+
+      return role
+    end
+
+    def role
+      noko.parent.css('p').text.tidy
+    end
+
+    def ministry
+      noko.xpath('preceding::h3[not(@class="title")][1]').text.to_s.tidy
     end
   end
 
   class Members
+    def members
+      super.select { |mem| mem[:position][/^(Ministro|Presidente)/] }
+    end
+
     def member_container
-      noko.css('.member')
+      noko.css('h4').each { |node| node.name = 'h3' } # Standardise these
+      noko.css('#pagina-50 h3.title')
     end
   end
 end
